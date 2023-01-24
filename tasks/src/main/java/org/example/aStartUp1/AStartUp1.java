5437a7097f2ab993ab0c9e9b0ef0e099a3bb5a83
Marcin <phppoznan@gmail.com>
2023-01-25 00:56:29 +0100

    feature: added he solution to the problem 'A Start up' 1 way

---
+++ tasks/src/main/java/org/example/aStartUp1/AStartUp1.java
@@ -0,0 +1,26 @@
+package org.example.aStartUp1;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * https://codeforces.com/problemset/problem/420/A
+ * Solution with StringBuilder, regex (Pattern, Matcher) and Scanner
+ */
+public class AStartUp1 {
+    Scanner sc = new Scanner(System.in);
+    String nameOfFirm = sc.nextLine();
+    //sc.close();
+
+    public boolean aStartUp1(String nameOfFirm) {
+        Pattern patternOfSymmetricalLetters = Pattern.compile("[AHIMOTUVWXY]*");
+        Matcher myMatcher = patternOfSymmetricalLetters.matcher(nameOfFirm);
+        boolean isFitted = myMatcher.matches();
+
+        StringBuilder myCheck = new StringBuilder(nameOfFirm).reverse();
+        String reversedName = String.valueOf(myCheck);
+
+        return isFitted && nameOfFirm.equals(reversedName);
+    }
+}
