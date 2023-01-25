2c2053fcfa6404c7a1ad88cee18f30def63f68f6
Marcin <phppoznan@gmail.com>
2023-01-26 00:16:35 +0100

    fix:  used immutable Set; added three variables instead of short snippets ; deleted comment

--- tasks/src/main/java/org/example/aStartUp2/AStartUp2.java
+++ tasks/src/main/java/org/example/aStartUp2/AStartUp2.java
@@ -1,37 +1,26 @@
 package org.example.aStartUp2;
 
-import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
 /**
  * https://codeforces.com/problemset/problem/420/A
  * Solution with Collections (Set), loops and Scanner
- * Question:
- * What is faster to read? A Set (HashSet) with complexity probably O(1) or a List (ArrayList) with complexity probably O(n)?
  */
 public class AStartUp2 {
     Scanner sc = new Scanner(System.in);
     String nameOfFirm = sc.nextLine();
-    //sc.close();
 
     public boolean aStartUp2(String nameOfFirm) {
-        Set<Character> symmetricalLetters = new HashSet<>();
-        symmetricalLetters.add('A');
-        symmetricalLetters.add('H');
-        symmetricalLetters.add('I');
-        symmetricalLetters.add('M');
-        symmetricalLetters.add('O');
-        symmetricalLetters.add('T');
-        symmetricalLetters.add('U');
-        symmetricalLetters.add('V');
-        symmetricalLetters.add('W');
-        symmetricalLetters.add('X');
-        symmetricalLetters.add('Y');
+        Set<Character> symmetricalLetters = Set.of('A', 'H', 'I', 'M', 'O', 'T', 'U', 'V', 'W', 'X', 'Y');
 
         boolean flag = true;
-        for (int i = 0; i < nameOfFirm.length() / 2 + 1; i++) {
-            if (nameOfFirm.charAt(i) != nameOfFirm.charAt(nameOfFirm.length() - 1 - i) || !symmetricalLetters.contains(nameOfFirm.charAt(i))) {
+        int lengthOfName = nameOfFirm.length();
+
+        for (int i = 0; i < lengthOfName / 2 + 1; i++) {
+            char frontLetterAtNameOfFirm = nameOfFirm.charAt(i);
+            char backLetterAtNameOfFirm = nameOfFirm.charAt(lengthOfName - 1 - i);
+            if (frontLetterAtNameOfFirm != backLetterAtNameOfFirm || !symmetricalLetters.contains(frontLetterAtNameOfFirm)) {
                 flag = false;
                 break;
             }
