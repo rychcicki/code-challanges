a289b39ac3b2bc97d762f191339bc15f43dcb9ec
Marcin <phppoznan@gmail.com>
2023-01-28 14:43:58 +0100

    feat: added the solution to the problem 'F Binary notation'

---
+++ tasks/src/main/java/org/example/fBinaryNotation/FBinaryNotation.java
@@ -0,0 +1,60 @@
+package org.example.fBinaryNotation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * https://codeforces.com/problemset/problem/188/F
+ * WARNING: Submit only in Roco language!!! It is not possible to submit code in Java.
+ */
+public class FBinaryNotation {
+    public static void main(String[] args) {
+        FBinaryNotation myMain = new FBinaryNotation();
+
+        FBinaryNotation.FastReader sc = new FastReader();
+        int n = sc.nextInt();
+        myMain.binaryNotation(n);
+    }
+
+    public int binaryNotation(int n) {
+        int[] binaryArr = new int[21];
+        int index = 0;
+        StringBuilder binaryBuilder = new StringBuilder();
+
+        while (n > 0) {
+            binaryArr[index++] = n % 2;
+            n /= 2;
+        }
+
+        for (int i = index - 1; i >= 0; i--) {
+            binaryBuilder.append(binaryArr[i]);
+        }
+        return Integer.parseInt(binaryBuilder.toString());
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
