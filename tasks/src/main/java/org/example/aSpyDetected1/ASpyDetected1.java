ed284626d35089964705aa8d2f7c3051a092994e
Marcin <phppoznan@gmail.com>
2023-01-29 13:40:38 +0100

    feat: added the solution to the problem 'A Spy detedted' - 1 way

--- tasks/src/main/java/org/example/aSpyDetected1/ASpyDetected1.java
+++ tasks/src/main/java/org/example/aSpyDetected1/ASpyDetected1.java
@@ -1,46 +1,53 @@
-package org.example;
+package org.example.aSpyDetected1;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 
 /**
  * https://codeforces.com/problemset/problem/1512/A
  */
 public class ASpyDetected1 {
-    public static void main(String[] args) {
-        ASpyDetected1.FastReader sc = new FastReader();
-        int numberOfTests = sc.nextInt();
+    FastReader sc = new FastReader();
+    int numberOfTests = sc.nextInt();
 
-        while (numberOfTests-- > 0) {
-            int lengthOfArray = sc.nextInt();
-            int[] arr = new int[lengthOfArray];
-            int unique = 0;
-
-            for (int i = 0; i < lengthOfArray; i++) {
-                int numbersInArray = sc.nextInt();
-                arr[i] = numbersInArray;
-            }
-
-            int key = Arrays.stream(arr)
-                    .boxed()
-                    .collect(Collectors.groupingBy(number -> number, Collectors.counting()))
-                    .entrySet()
-                    .stream()
-                    .filter(count -> count.getValue() == 1)
-                    .map(Map.Entry::getKey)
-                    .findFirst()
-                    .get();
-
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[j] == key) {
-                    unique = j + 1;
-                }
-            }
-            System.out.println(unique);
+    void loop() {
+        for (int i = 1; i <= numberOfTests; i++) {
+            aSpyDetected1(sc);
         }
+    }
+
+    int aSpyDetected1(FastReader sc) {
+        int unique = 0;
+
+        int lengthOfArray = sc.nextInt();
+        int[] arr = new int[lengthOfArray];
+
+        for (int i = 0; i < lengthOfArray; i++) {
+            int numbersInArray = sc.nextInt();
+            arr[i] = numbersInArray;
+        }
+
+        int key = Arrays.stream(arr)
+                .boxed()
+                .collect(Collectors.groupingBy(number -> number, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(count -> count.getValue() == 1)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .get();
+
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] == key) {
+                unique = j + 1;
+            }
+        }
+        return unique;
     }
 
     static class FastReader {
