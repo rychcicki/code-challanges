e3af6107f7730c6475fd28149e09ab387d6fc960
Marcin <phppoznan@gmail.com>
2023-02-12 16:29:38 +0100

    chore: added link to the problem A Sereja and Dima

--- tasks/src/main/java/org/example/aSerejaAndDima/ASerejaAndDima.java
+++ tasks/src/main/java/org/example/aSerejaAndDima/ASerejaAndDima.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ *  https://codeforces.com/contest/381/problem/A
+ */
 public class ASerejaAndDima {
     FastReader sc = new FastReader();
     short numberOfFreeCards = sc.nextShort();
