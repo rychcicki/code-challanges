aee9f113e939731671077279a815b8b15de4ef6d
Marcin <phppoznan@gmail.com>
2023-03-02 23:41:19 +0100

    tests: added tests template with logs

---
+++ tasks/src/test/java/org/example/aProblemAboutEquation/AProblemAboutEquation3Test.java
@@ -0,0 +1,43 @@
+package org.example.aProblemAboutEquation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+class AProblemAboutEquation3Test {
+    @Test
+    void shouldReturnFalseWhenNumberOfFriendsLessThanTwo() {
+        //given - arrange
+        log.info("temp");
+        //when - act
+
+        //then - assert
+    }
+
+    @Test
+    void shouldReturnFalseWhenNumberOfFriendsGreaterThan100() {
+        //given - arrange
+
+        //when - act
+
+        //then - assert
+    }
+
+    @Test
+    void shouldReturnFalseWhenNumberOfMillilitersOfTheDrinkInTheBottleLessThanOne() {
+        //given - arrange
+
+        //when - act
+
+        //then - assert
+    }
+
+    @Test
+    void shouldReturnFalseWhenNumberOfMillilitersOfTheDrinkInTheBottleGreaterThan100() {
+        //given - arrange
+
+        //when - act
+
+        //then - assert
+    }
+}
